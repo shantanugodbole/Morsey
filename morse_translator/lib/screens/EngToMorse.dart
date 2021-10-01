0bf89704a33a5b3463103a43f850a8e76a2f1482
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-10-01 18:12:00 +0530

    Fix overflow issues

--- morse_translator/lib/screens/EngToMorse.dart
+++ morse_translator/lib/screens/EngToMorse.dart
@@ -31,13 +31,15 @@ class _EngToMorseState extends State<EngToMorse> {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
     return Material(
       color: Color(0xFFFEEAE6),
-      child: new Container(
+      child: new SingleChildScrollView(
         child: Column(
           children: [
             Container(
-              padding: EdgeInsets.all(50),
+              padding: EdgeInsets.symmetric(vertical: height/15, horizontal: 20),
               child: Text(
                 "Translate from English to Morse",
                 textAlign: TextAlign.center,
@@ -46,14 +48,14 @@ class _EngToMorseState extends State<EngToMorse> {
               ),
             ),
             Container(
-              margin: EdgeInsets.all(15),
+              margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
               child: TextField(
-                style: TextStyle(fontSize: 22),
+                style: TextStyle(fontSize: 16),
                 controller: inputController,
                 maxLines: 6,
                 decoration: InputDecoration(
                   hintStyle: TextStyle(
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
                   hintText: "Enter your secret message!",
                   filled: true,
@@ -68,19 +70,23 @@ class _EngToMorseState extends State<EngToMorse> {
                 cursorColor: Colors.brown,
               ),
             ),
-            RaisedButton(
-              onPressed: () {
-                translateToMorse();
-                FocusManager.instance.primaryFocus.unfocus();
-              },
-              child: Text(
-                "Encode",
-                style:
-                    GoogleFonts.allan(color: Color(0xFF442C2E), fontSize: 34),
-              ),
-              color: Color(0xFFFEDBD0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: MaterialButton(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                onPressed: () {
+                  translateToMorse();
+                  FocusManager.instance.primaryFocus.unfocus();
+                },
+                child: Text(
+                  "Encode",
+                  style:
+                      GoogleFonts.allan(color: Color(0xFF442C2E), fontSize: 22),
+                ),
+                color: Color(0xFFFEDBD0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
               ),
             ),
             SizedBox(
@@ -89,12 +95,12 @@ class _EngToMorseState extends State<EngToMorse> {
             Container(
               margin: EdgeInsets.all(15),
               child: TextField(
-                style: TextStyle(fontSize: 22),
+                style: TextStyle(fontSize: 16),
                 controller: myController,
                 maxLines: 6,
                 decoration: InputDecoration(
                   hintStyle: TextStyle(
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
                   hintText: "Your message encoded in Morse will appear here",
                   filled: true,
@@ -109,69 +115,75 @@ class _EngToMorseState extends State<EngToMorse> {
                 cursorColor: Colors.brown,
               ),
             ),
-            RaisedButton(
-              onPressed: () {
-                inputText = myController.text;
-                ClipboardManager.copyToClipBoard(myController.text);
-                myController.text = "Message copied to clipboard!";
-                // showSnackBar();
-              },
-              child: Text(
-                "Copy",
-                style:
-                    GoogleFonts.allan(color: Color(0xFF442C2E), fontSize: 34),
-              ),
-              color: Color(0xFFFEDBD0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              child: MaterialButton(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                onPressed: () {
+                  inputText = myController.text;
+                  ClipboardManager.copyToClipBoard(myController.text);
+                  myController.text = "Message copied to clipboard!";
+                  // showSnackBar();
+                },
+                child: Text(
+                  "Copy",
+                  style:
+                      GoogleFonts.allan(color: Color(0xFF442C2E), fontSize: 22),
+                ),
+                color: Color(0xFFFEDBD0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
               ),
             ),
-            SizedBox(
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  child: RaisedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      "Back",
-                      style: GoogleFonts.allan(
-                          color: Color(0xFF442C2E), fontSize: 34),
-                    ),
-                    color: Color(0xFFFEDBD0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                ),
-                Container(
-                  child: RaisedButton(
-                    onPressed: () {
-                      if (inputText == "") {
-                        inputText = myController.text;
-                        ClipboardManager.copyToClipBoard(myController.text);
-                        myController.text = "Message copied to clipboard!";
-                        Share.share(inputText);
-                      } else {
-                        Share.share(inputText);
-                      }
-                    },
-                    child: Text(
-                      "Share",
-                      style: GoogleFonts.allan(
-                          color: Color(0xFF442C2E), fontSize: 34),
-                    ),
-                    color: Color(0xFFFEDBD0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 18.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: MaterialButton(
+                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        "Back",
+                        style: GoogleFonts.allan(
+                            color: Color(0xFF442C2E), fontSize: 22),
+                      ),
+                      color: Color(0xFFFEDBD0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                  Container(
+                    child: MaterialButton(
+                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      onPressed: () {
+                        if (inputText == "") {
+                          inputText = myController.text;
+                          ClipboardManager.copyToClipBoard(myController.text);
+                          myController.text = "Message copied to clipboard!";
+                          Share.share(inputText);
+                        } else {
+                          Share.share(inputText);
+                        }
+                      },
+                      child: Text(
+                        "Share",
+                        style: GoogleFonts.allan(
+                            color: Color(0xFF442C2E), fontSize: 22),
+                      ),
+                      color: Color(0xFFFEDBD0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
